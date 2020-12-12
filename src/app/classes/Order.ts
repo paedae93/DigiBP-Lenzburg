4fbc98b06912a6f149b39715e2e15d715fe1ec3e
Patrick Voirol <patrick.voirol@gmail.com>
2020-12-12 11:38:00 +0100

    correct order class

--- src/app/classes/Order.ts
+++ src/app/classes/Order.ts
@@ -1,12 +1,12 @@
 export class Order{
-    order_id : number;
-    customer_id : number;
-    prescription_id : number;
-    product_id : number;
-    price : number;
-    purchase_date : String
-    payment_method : String;
-    status : String;
-    process_id : String;
-    business_key : String;
+    order_id : number | undefined;
+    customer_id : number | undefined;
+    prescription_id : number | undefined;
+    product_id : number | undefined;
+    price : number | undefined;
+    purchase_date : String | undefined;
+    payment_method : String | undefined;
+    status : String | undefined;
+    process_id : String | undefined;
+    business_key : String | undefined;
 }
